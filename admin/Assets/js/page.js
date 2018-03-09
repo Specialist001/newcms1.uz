7bdd64564558856a14ca57aae3193790f3e27a66
Specialist001 <darking-uz@yandex.ru>
2018-03-10 00:21:01 +0500

    0:20 U24

--- admin/Assets/js/page.js
+++ admin/Assets/js/page.js
@@ -24,3 +24,5 @@ var page = {
 
     }
 };
+
+console.log(page);
--- admin/Route.php
+++ admin/Route.php
@@ -10,6 +10,6 @@ $this->router->add('logout', '/admin/logout/', 'AdminController:logout');
 
 $this->router->add('pages', '/admin/pages/', 'PageController:listing');
 $this->router->add('page-create', '/admin/pages/create/', 'PageController:create');
-$this->router->add('page-add', '/admin/pages/add/', 'PageController:add', 'POST');
+$this->router->add('page-add', '/admin/page/add/', 'PageController:add', 'POST');
 
 //exit;
