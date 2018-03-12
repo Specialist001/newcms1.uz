b1d97e0795d978177098e0accfb78f5f1d756919
Specialist001 <darking-uz@yandex.ru>
2018-03-12 23:50:58 +0500

    23:50

--- engine/Core/Database/Connection.php
+++ engine/Core/Database/Connection.php
@@ -5,10 +5,6 @@ namespace Engine\Core\Database;
 use \PDO;
 use Engine\Core\Config\Config;
 
-/**
- * Class Connection
- * @package Engine\Core\Database
- */
 class Connection
 {
     private $link;
@@ -24,11 +20,12 @@ class Connection
     /**
      * @return $this
      */
-    private function connect()
-    {
-        $config = Config::group('database');
+    private function connect(){
+        $config = Config::file('database');
 
-        $dsn = 'mysql:host=' .$config['host'] .';dbname=' .$config['db_name'] .';charset=' .$config['charset'];
+        //print_r($config);
+
+        $dsn = 'mysql:host='.$config['host'].';dbname='.$config['db_name'].';charset='.$config['charset'];
 
         $this->link = new PDO($dsn, $config['username'], $config['password']);
 
@@ -40,39 +37,32 @@ class Connection
      * @param array $values
      * @return mixed
      */
-    public function execute($sql, $values = [])
-    {
+    public function execute($sql, $values = []){
         $sth = $this->link->prepare($sql);
 
         return $sth->execute($values);
     }
 
+
     /**
      * @param $sql
      * @param array $values
-     * @param int $statement
      * @return array
      */
-    public function query($sql, $values = [], $statement = PDO::FETCH_OBJ)
-    {
+    public function query($sql, $values = []){
         $sth = $this->link->prepare($sql);
 
         $sth->execute($values);
 
-        $result = $sth->fetchAll($statement);
+        $result = $sth->fetchAll(PDO::FETCH_ASSOC);
 
         if($result === false){
             return [];
         }
-
         return $result;
     }
 
-    /**
-     * @return int
-     */
-    public function lastInsertId()
-    {
+    public function lastInsertId(){
         return $this->link->lastInsertId();
     }
 }
